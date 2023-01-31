fc295ee1378d5046fdaafde41e79a63029fa5eeb
Sofi <belusofi17@gmail.com>
2023-01-31 00:16:36 -0500

    Initial commit

---
+++ src/main/resources/db.migration/V2__insertar_datos.sql
@@ -0,0 +1,4 @@
+insert into books(isbn, title, author, price) values ('66-11','title1','1',20);
+insert into books(isbn, title, author, price) values ('66-22','title2','1',20);
+insert into books(isbn, title, author, price) values ('66-33','title3','1',20);
+insert into books(isbn, title, author, price) values ('66-44','title4','1',20);
